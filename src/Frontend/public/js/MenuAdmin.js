3f51bdfc1e570e6904f4640f749737fc20839a79
Lederback <renatosmachado2@gmail.com>
2022-06-06 16:27:57 -0300

    Versão Inicial Integração API

--- src/Frontend/public/js/MenuAdmin.js
+++ src/Frontend/public/js/MenuAdmin.js
@@ -1,8 +1,29 @@
+let searchType = "Estabelecimento";
+
+function changeSearchType(){
+    switch(document.getElementById("search-type").value){
+        case "Estabelecimento":
+            searchType = "Estabelecimento";
+            $("#partner-list").css("display", "block");
+            $("#invoice-list").css("display", "none");
+            break;
+        case "Pendentes":
+            searchType = "Pendentes";
+            $("#partner-list").css("display", "none");
+            $("#invoice-list").css("display", "block");
+            break;
+        case "Passadas":
+            searchType = "Passadas";
+            $("#partner-list").css("display", "none");
+            $("#invoice-list").css("display", "block");
+            break;
+    }
+}
+
 $(document).ready(function(){
     //Taking ranking info from database.
     $.get("http://127.0.0.1:5555/getRanking", function(resultado){
         var objeto = JSON.parse(resultado);
-        console.log(Object.keys(objeto).length);
         for(i = 0; i < Object.keys(objeto).length; i ++){
             $("#ranking-table").append(`<tr>
                                             <td class="ranking-table-position">` + (i + 1) + `</td>
@@ -17,8 +38,6 @@ $(document).ready(function(){
     //Taking the general info section from database.
     $.get("http://127.0.0.1:5555/getGeneralVision", function(resultado){
         var objeto = JSON.parse(resultado);
-        console.log(objeto);
-        console.log(Object.keys(objeto).length);
         $("#total-amount-anticipations").html(objeto[0].TotalDeAntecipações);
         $("#total-amount-advance").html(objeto[0].ValorTotalAntecipado);
         $("#total-taxed-amount").html(objeto[0].ValorTotalTaxado);
