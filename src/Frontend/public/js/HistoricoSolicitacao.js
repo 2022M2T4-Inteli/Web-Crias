51381bf4ecf7010645c05dae8026411bfa4cb3af
Lederback <renatosmachado2@gmail.com>
2022-06-14 09:10:52 -0300

    Versão Final Tela Administrador

--- src/Frontend/public/js/HistoricoSolicitacao.js
+++ src/Frontend/public/js/HistoricoSolicitacao.js
@@ -5,8 +5,39 @@ $(document).ready(function(){
     //Taking ranking info from database.
     $.get(url1, function(resultado){
         var objeto = JSON.parse(resultado);
+        var aux = 1;
         for(i = 0; i < Object.keys(objeto).length; i ++){
-            $("#pending-table").append(`<tr>
+            if (aux == 1){
+                $("#pending-table").append(`<tr>
+                                                <td class="nf">` + objeto[i].NotaFiscal + `</td>
+                                                <td class="total-value">` + (objeto[i].ValorRecebido).toFixed(2) + `</td>
+                                                <td class="receive-value">` + (objeto[i].ValorTaxado).toFixed(2) + `</td>
+                                                <td class="antecipation-type">` + objeto[i].TipoAntecipação + `</td>
+                                                <td class="date">` + objeto[i].Data + `</td>
+                                                <td class="status">` + objeto[i].Status + `</td>
+                                            </tr>`);
+            }
+            else{
+                $("#pending-table").append(`<tr>
+                                                <td class="nf" style="background-color: #F2F2F2">` + objeto[i].NotaFiscal + `</td>
+                                                <td class="total-value" style="background-color: #F2F2F2">` + (objeto[i].ValorRecebido).toFixed(2) + `</td>
+                                                <td class="receive-value" style="background-color: #F2F2F2">` + (objeto[i].ValorTaxado).toFixed(2) + `</td>
+                                                <td class="antecipation-type" style="background-color: #F2F2F2">` + objeto[i].TipoAntecipação + `</td>
+                                                <td class="date" style="background-color: #F2F2F2">` + objeto[i].Data + `</td>
+                                                <td class="status" style="background-color: #F2F2F2">` + objeto[i].Status + `</td>
+                                            </tr>`);
+            }
+
+            aux = -aux;
+        }
+    });
+
+    $.get(url2, function(resultado){
+        var objeto = JSON.parse(resultado);
+        aux = 1;
+        for(i = 0; i < Object.keys(objeto).length; i ++){
+            if (aux == 1){
+                $("#past-table").append(`<tr>
                                             <td class="nf">` + objeto[i].NotaFiscal + `</td>
                                             <td class="total-value">` + (objeto[i].ValorRecebido).toFixed(2) + `</td>
                                             <td class="receive-value">` + (objeto[i].ValorTaxado).toFixed(2) + `</td>
@@ -14,20 +45,19 @@ $(document).ready(function(){
                                             <td class="date">` + objeto[i].Data + `</td>
                                             <td class="status">` + objeto[i].Status + `</td>
                                         </tr>`);
-        }
-    });
+            }
+            else{
+                $("#past-table").append(`<tr>
+                                            <td class="nf" style="background-color: #F2F2F2">` + objeto[i].NotaFiscal + `</td>
+                                            <td class="total-value" style="background-color: #F2F2F2">` + (objeto[i].ValorRecebido).toFixed(2) + `</td>
+                                            <td class="receive-value" style="background-color: #F2F2F2">` + (objeto[i].ValorTaxado).toFixed(2) + `</td>
+                                            <td class="antecipation-type" style="background-color: #F2F2F2">` + objeto[i].TipoAntecipação + `</td>
+                                            <td class="date" style="background-color: #F2F2F2">` + objeto[i].Data + `</td>
+                                            <td class="status" style="background-color: #F2F2F2">` + objeto[i].Status + `</td>
+                                        </tr>`);
+            }
 
-    $.get(url2, function(resultado){
-        var objeto = JSON.parse(resultado);
-        for(i = 0; i < Object.keys(objeto).length; i ++){
-            $("#past-table").append(`<tr>
-                                        <td class="nf">` + objeto[i].NotaFiscal + `</td>
-                                        <td class="total-value">` + (objeto[i].ValorRecebido).toFixed(2) + `</td>
-                                        <td class="receive-value">` + (objeto[i].ValorTaxado).toFixed(2) + `</td>
-                                        <td class="antecipation-type">` + objeto[i].TipoAntecipação + `</td>
-                                        <td class="date">` + objeto[i].Data + `</td>
-                                        <td class="status">` + objeto[i].Status + `</td>
-                                    </tr>`);
-    }
+            aux = -aux;
+        }
     });
 });
